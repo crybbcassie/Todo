7224aeee5fb06ae3c9a43fd291d2d4f667550bf9
Cassie <crybbcassie@gmail.com>
2023-05-23 22:53:43 +0300

    update api functions

--- src/components/API/Service.jsx
+++ src/components/API/Service.jsx
@@ -37,3 +37,43 @@ export async function login(userLoginData) {
       console.log(e);
     });
 }
+
+export async function createTodos(token, todo) {
+  await axios
+    .post(`https://first-node-js-app-r.herokuapp.com/api/todos`, todo, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((e) => {
+      console.log(e);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
+export async function updateTodo(newTodo, token, id) {
+  await axios.patch(
+    `https://first-node-js-app-r.herokuapp.com/api/todos/${id}`,
+    {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
+}
+
+export async function deleteTodo(id, token) {
+  await axios.delete(
+    `https://first-node-js-app-r.herokuapp.com/api/todos/${id}`,
+    {
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
+}
