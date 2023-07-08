8889204c33dd2ba0b4811a444ae0245f601d56df
Cassie <crybbcassie@gmail.com>
2023-07-08 14:09:43 +0300

    add redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Navigation from './navigation/Navigation';
+// import { AuthContext } from "./context/Context";
+import { useState } from "react";
+
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
 
   return (
+   
       <div className="App">
+        {/* <AuthContext.Provider
+        value={{
+          isAuth,
+          setIsAuth,
+        }}
+      > */}
         <Navigation />
+        {/* </AuthContext.Provider> */}
       </div>
   );
 }
